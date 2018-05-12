fb3d827df79de8c018d105f2d86c0a3dcb301786
Deniz Çıkış <33332471+denizcks@users.noreply.github.com>
2018-05-12 20:04:05 +0300

    Add files via upload

---
+++ Codes/Homepage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Homepage : BaseClass
+{
+    public static string logFileName = "page_HomePage";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        sIpAddress = HttpContext.Current.Request.UserHostAddress;
+        LogMessage("### Information ### => ipAddress = " + sIpAddress, "Ip List", logFileName);
+    }
+}
